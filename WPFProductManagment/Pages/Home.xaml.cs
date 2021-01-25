7807f0a6712cbcd70110d465f48419c3149acfab
Hamid Keshmiri-Neghab <Hamid.Keshmiri-Neghab@konplan.cz>
2021-01-25 14:50:24 +0100

    version3 * changes in Home

--- WPFProductManagment/Pages/Home.xaml.cs
+++ WPFProductManagment/Pages/Home.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -52,6 +53,46 @@ namespace WPFProductManagment.View.Pages
       EmployessPanel.Visibility = Visibility.Collapsed;
       ProductsPanel.Visibility = Visibility.Visible;
       CustomersPanel.Visibility = Visibility.Collapsed;
+    }
+
+    private void Twitter_OnClick(object sender, RoutedEventArgs e)
+    {
+      var url = new ProcessStartInfo("http://www.twitter.com")
+      {
+        UseShellExecute = true,
+        Verb = "open"
+      };
+      System.Diagnostics.Process.Start(url);
+    }
+
+    private void Facebook_OnClick(object sender, RoutedEventArgs e)
+    {
+      var url = new ProcessStartInfo("http://www.facebook.com")
+      {
+        UseShellExecute = true,
+        Verb = "open"
+      };
+      Process.Start(url);
+    }
+
+    private void Linkedin_OnClick(object sender, RoutedEventArgs e)
+    {
+      var url = new ProcessStartInfo("http://www.linkedin.com")
+      {
+        UseShellExecute = true,
+        Verb = "open"
+      };
+      Process.Start(url);
+    }
+
+    private void Blogger_OnClick(object sender, RoutedEventArgs e)
+    {
+      var url = new ProcessStartInfo("http://www.blogger.com")
+      {
+        UseShellExecute = true,
+        Verb = "open"
+      };
+      Process.Start(url);
     }
   }
 }
